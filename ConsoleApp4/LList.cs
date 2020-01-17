75b5e9175bf54fa1af1c59e9180842df880ad1e7
Tony Collins <tjc@eltham-college.org.uk>
2020-01-17 12:53:45 +0000

    Add project files.

---
+++ ConsoleApp4/LList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class LList
+    {
+        private Node head;
+
+        public LList()
+        {
+            head = null;
+
+        }
+
+        public void addList(string name)
+        {
+            Node s;
+            s = new Node();
+
+            s.name = name;
+            s.next = null;
+            Node temp;
+            temp = head;
+            if (temp == null)
+            {
+                head = s;
+            }
+            else
+            {
+
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+
+                }
+                temp.next = s;
+
+
+            }
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp4/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Node
+    {
+        public Node next;
+        public string name;
+
+
+    }
+}
